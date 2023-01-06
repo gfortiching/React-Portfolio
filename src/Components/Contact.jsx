7936796a406c7cc36b99e2245c416db7490cb8b6
Gab <gabrielfabilena@gmail.com>
2023-01-06 20:26:09 +0800

    Update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
     <section className="main">
         <h1 className="arvo text-center text-sm-start mb-5 mt-2'">Contact</h1>
         <div className="contactContainer">
-
+            <p>Hello</p>
         </div>
     </section>
     </>
